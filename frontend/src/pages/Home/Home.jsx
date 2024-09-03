29aa1317de1caebf5806b3490fac44517840c227
prathammhavale <prathammhavale38@gmail.com>
2024-09-03 14:18:56 +0530

    redesign

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,7 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import CardSection from "../../components/CardSection/CardSection";
-import LogoBar from "../../components/LogoBar/LogoBar";
-import CategorySection from "../../components/CategorySection/CategorySection";
-import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
-  return (
-    <div className="container">
-      <LogoBar />
-      <Navbar />
-      <CategorySection />
-      <Footer />
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default Home;
